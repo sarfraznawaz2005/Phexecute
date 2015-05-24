8a9ac697e2009800cfac37e1fff92af5ba5d2cda
Sarfraz <sarfraznawaz2005@gmail.com>
2015-05-24 14:23:58 +0500

    added screenshots

--- storage/temp/file.php
+++ storage/temp/file.php
@@ -1,10 +1,26 @@
-<?php // path to file OR directory to test
-$path = __DIR__ . '/../../storage/phpunit/tests';
-// path to phpunit executable
-$phpunit = __DIR__ . '/../../vendor/bin/phpunit';
-// run tests
-exec($phpunit . ' ' . $path . ' 2>&1', $result);
-// show results
-echo '<pre>';
-out($result);
-echo '</pre>';
+<?php function benchmark($function, $args = null, $iterations = 1, $timeout = 0)
+{
+    set_time_limit($timeout);
+
+    if (is_callable($function) === true) {
+        list($usec, $sec) = explode(" ", microtime());
+        $start = ((float) $usec + (float) $sec);
+
+        for ($i = 1; $i <= $iterations; ++ $i) {
+            call_user_func_array($function, (array) $args);
+        }
+
+        list($usec, $sec) = explode(" ", microtime());
+        $end = ((float) $usec + (float) $sec);
+
+        return round(($end - $start), 4);
+    }
+
+    return false;
+}
+
+$result = benchmark(function () {
+    sleep(3);
+});
+
+echo $result;
