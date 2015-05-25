0357b50a897c32df9f3cf46869cc766efa0ca562
Sarfraz <sarfraznawaz2005@gmail.com>
2015-05-25 15:30:56 +0500

    added cache folder to gitignore

--- storage/temp/file.php
+++ storage/temp/file.php
@@ -1,12 +1,26 @@
-<?php class TestSomething extends UnitTestCase
+<?php function benchmark($function, $args = null, $iterations = 1, $timeout = 0)
 {
-    
-    function testValueisTrue()
-    {
-        $value = true;
-        $this->assertTrue($value);
+    set_time_limit($timeout);
+
+    if (is_callable($function) === true) {
+        list($usec, $sec) = explode(" ", microtime());
+        $start = ((float) $usec + (float) $sec);
+
+        for ($i = 1; $i <= $iterations; ++ $i) {
+            call_user_func_array($function, (array) $args);
+        }
+
+        list($usec, $sec) = explode(" ", microtime());
+        $end = ((float) $usec + (float) $sec);
+
+        return round(($end - $start), 4);
     }
+
+    return false;
 }
 
-$test = new TestSomething();
-$test->run(new HtmlReporter());
+$result = benchmark(function () {
+    sleep(3);
+});
+
+echo $result;
